bd3e52fe4d6d496d5afd90ef5dddd894c79f3e11
Devon Hogencamp <devon.hogencamp@west-mec.org>
2016-08-15 15:25:54 -0700

    got all the css and started working on js

---
+++ script.js
@@ -0,0 +1,10 @@
+var numberOfFaces = 5;
+
+var left = document.getElementById("left");
+
+function generateFaces() {
+    while (numberOfFaces <= 5) {
+        var x = Math.random();
+        
+    }
+}
