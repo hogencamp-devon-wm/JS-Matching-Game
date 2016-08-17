d4f895af3fc235bed00c21493d0a12714d85dcd9
Devon Hogencamp <devon.hogencamp@west-mec.org>
2016-08-17 13:03:14 -0700

    All done

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-var numberOfFaces = 5;
-
-var left = document.getElementById("left");
-
 function generateFaces() {
-    while (numberOfFaces <= 5) {
-        var x = Math.random();
-        
+    var numberOfFaces = 5;
+    var theLeftSide = document.getElementById("leftSide");
+    for (i = 0; i < numberOfFaces; ++ i) {
+        car = document.createElement("img");
+        car.src = "car.jpg";
+        car.style.top = Math.floor(Math.random() * 401) + "px";
+        car.style.left = Math.floor(Math.random() * 401) + "px";
+        theLeftSide.appendChild(car);
+    }
+    var theRightSide = document.getElementById("rightSide");
+    leftSideImages = theLeftSide.cloneNode(true);
+    leftSideImages.removeChild(leftSideImages.lastChild);
+    theRightSide.appendChild(leftSideImages);
+
+    var theBody = document.getElementsByTagName("body")[0];
+    theLeftSide.lastChild.onclick = function nextLevel (event){
+        event.stopPropagation();
+        numberOfFaces += 5;
+        generateFaces();
+    };
+    theBody.onclick = function gameOver() {
+        alert("Game Over");
+        theBody.onclick = null;
+        theLeftSide.lastChild.onclick = null;
     }
 }
